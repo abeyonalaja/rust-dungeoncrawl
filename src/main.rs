2b9ae39d82bb8a99947771cd9ae56edea77cc469
abey onalaja <abey.onalaja@gmail.com>
2022-12-10 02:15:13 +0000

    render the map

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,29 @@ mod prelude {
     pub use crate::map::*;
 }
 use prelude::*;
+struct State {
+    map: Map,
+}
 
-fn main() {
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.map.render(ctx);
+    }
+}
+
+fn main() -> BError {
     println!("Hello, world!");
+    let context = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(context, State::new())
 }
